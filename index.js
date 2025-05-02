b555e38102935aa5ed348ec6fa49a347cb023d4c
mattiabonazzii <mattib249@gmail.com>
2025-05-02 15:17:59 +0200

    Aggiunta di moment e di sprechi oggi

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const crypto = require('crypto');
 const app = express();
 require('dotenv').config();
+const moment = require('moment');
 const port = 4000;
 
 //quantità sprecate al secondo
@@ -12,10 +13,30 @@ const emissioniCO2PerSecondo = 1319; // tonnellate
 const avvio = Date.now();
 const key = process.env.KEY;
 
-app.get('/sprechi/:chiave', (req, res) => {
+//risorse sprecate dall'avvio del server
+app.get('/sprechi-inizio', (req, res) => {
   const adesso = Date.now();
   const secondiPassati = Math.floor((adesso - avvio) / 1000);
-  const chiave = req.params.chiave;
+  const chiave = req.headers['chiave'];
+
+  let hash = crypto.createHash('sha256').update(chiave).digest('hex');
+
+  if(hash != key){
+    res.status(401).json({errore: "chiave non valida"});
+    return;
+  }
+
+  res.status(200).json({
+    tempo_passato: `${secondiPassati} secondi`,
+    cibo_sprecato: `${(sprecoCiboPerSecondo * secondiPassati).toFixed(2)} tonnellate`,
+    acqua_sprecata: `${(sprecoAcquaPerSecondo * secondiPassati).toLocaleString()} litri`,
+    co2_emessa: `${(emissioniCO2PerSecondo * secondiPassati).toFixed(2)} tonnellate`
+  });
+});
+
+app.get('/sprechi-oggi', (req, res) => {
+  const secondiPassati = (moment() - moment().startOf('day')) / 1000;
+  const chiave = req.headers['chiave'];
 
   let hash = crypto.createHash('sha256').update(chiave).digest('hex');
 
@@ -35,7 +56,3 @@ app.get('/sprechi/:chiave', (req, res) => {
 app.listen(port, () => {
   console.log(`API attiva su http://localhost:${port}`);
 });
-
-
-let hash = crypto.createHash('sha256', 'qwerty');
-hash = hash.digest('hex');
